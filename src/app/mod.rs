189460457266d3f29894e3f4a0841d014abee26e
Degra02 <filippodegrandi02@gmail.com>
2024-04-07 11:27:09 +0200

    Refactor

---
+++ src/app/mod.rs
@@ -0,0 +1,37 @@
+use std::io::{stdout, Stdout};
+
+use crossterm::{
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{backend::CrosstermBackend, Terminal};
+
+use self::tui::App;
+
+pub mod tui;
+
+pub type Tui = Terminal<CrosstermBackend<Stdout>>;
+
+fn init() -> std::io::Result<Tui> {
+    // execute!(stdout(), EnterAlternateScreen)?;
+    let _ = stdout().execute(EnterAlternateScreen);
+    enable_raw_mode()?;
+    Terminal::new(CrosstermBackend::new(stdout()))
+}
+
+fn restore() -> std::io::Result<()> {
+    let _ = stdout().execute(LeaveAlternateScreen);
+    disable_raw_mode()?;
+    Ok(())
+}
+
+pub fn start_workspace(path: &str, workspace: &str) -> std::io::Result<()> {
+    App::from_config(path)?.start_workspace(workspace)
+}
+
+pub fn run_tui(path: &str) -> std::io::Result<()> {
+    let mut terminal = init()?;
+    let _ = tui::App::from_config(path)?.run(&mut terminal);
+    restore()?;
+    Ok(())
+}
---
+++ src/app/tui.rs
@@ -0,0 +1,151 @@
+use std::{
+    collections::HashMap,
+    process::Command,
+};
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    layout::Alignment,
+    style::{Color, Modifier, Style},
+    text::{Line, Span},
+    widgets::{block::Title, Block, Borders, List, ListItem},
+    Frame,
+};
+use serde::{Deserialize, Serialize};
+
+use crate::workspaces::WorkSpace;
+
+use super::Tui;
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct App {
+    pub workspaces: HashMap<String, WorkSpace>,
+
+    #[serde(skip)]
+    pub ws_names: Vec<String>,
+
+    #[serde(skip)]
+    index: usize,
+    #[serde(skip)]
+    exit: bool,
+}
+
+impl App {
+    #[allow(unused)]
+    pub fn new(workspaces: HashMap<String, WorkSpace>) -> Self {
+        let mut ws_names = Vec::<String>::new();
+        for (name, ws) in workspaces.iter() {
+            ws_names.push(name.clone());
+        }
+
+        Self {
+            workspaces,
+            ws_names,
+            index: 0,
+            exit: false,
+        }
+    }
+
+    pub fn from_config(path: &str) -> std::io::Result<Self> {
+        let data = std::fs::read_to_string(path)?;
+        let mut app: App = serde_json::from_str(&data)?;
+
+        let mut ws_names = Vec::<String>::new();
+        for (name, _) in &app.workspaces {
+            ws_names.push(name.clone());
+        }
+        // Sorting alfabetically
+        ws_names.sort();
+        app.ws_names = ws_names;
+
+        Ok(app)
+    }
+
+    pub fn run(&mut self, terminal: &mut Tui) -> std::io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
+        }
+
+        Ok(())
+    }
+
+    pub fn start_workspace(&mut self, workspace: &str) -> std::io::Result<()> {
+        let ws = &self.workspaces[workspace];
+        Command::new("sh")
+            .arg("-c")
+            .arg(ws.command())
+            .output()?;
+
+        Ok(())
+    }
+
+    fn render_frame(&mut self, f: &mut Frame) {
+        let title = Title::from("WorkSpaces");
+        let block = Block::default()
+            .borders(Borders::ALL)
+            .title(title)
+            .title_alignment(Alignment::Center);
+
+        let mut list = Vec::<ListItem>::new();
+
+        for (i, ws_name) in self.ws_names.iter().enumerate() {
+            let style = if i == self.index {
+                Style::default().fg(Color::Yellow)
+            } else {
+                Style::default()
+            };
+            list.push(ListItem::new(
+                Line::from(Span::from(format!("{}", ws_name)))
+                    .alignment(Alignment::Center)
+                    .style(style),
+            ));
+        }
+
+        let list = List::new(list).block(block).highlight_style(
+            Style::default()
+                .add_modifier(Modifier::BOLD)
+                .add_modifier(Modifier::ITALIC),
+        );
+        f.render_widget(list, f.size());
+    }
+
+    fn handle_events(&mut self) -> std::io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_events(key_event)
+            }
+            _ => Ok(()),
+        }
+    }
+
+    fn handle_key_events(&mut self, key_event: KeyEvent) -> std::io::Result<()> {
+        match key_event.code {
+            KeyCode::Char('j') => {
+                if self.index < self.workspaces.len() - 1 {
+                    self.index += 1;
+                }
+            }
+            KeyCode::Char('k') => {
+                if self.index > 0 {
+                    self.index -= 1;
+                }
+            }
+            KeyCode::Enter => self.execute_selected()?,
+            KeyCode::Char('q') | KeyCode::Esc => self.exit = true,
+            _ => {}
+        }
+
+        Ok(())
+    }
+
+    fn execute_selected(&mut self) -> std::io::Result<()> {
+        let ws_name = &self.ws_names[self.index];
+        Command::new("sh")
+            .arg("-c")
+            .arg(self.workspaces[ws_name].command())
+            .output()?;
+
+        Ok(())
+    }
+}
